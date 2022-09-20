a75c4fdb6cab1e2b4406fdb374cfc0b0e54fd781
wznmickey <first@wznmickey.com>
2022-09-20 23:20:43 +0800

    fix: add space and exit response as well as add one empty line to make it compiled in joj

--- main.c
+++ main.c
@@ -4,10 +4,11 @@
 int main() {
   char input[2048];
   while (true) {
-    printf("mumsh $");
+    printf("mumsh $ ");
     memset(input, 0, sizeof(char) * 2048);
     fgets(input, 2048 - 1, stdin);
     if (strcmp(input, "exit\n") == 0) {
+      printf("exit\n");
       return 0;
     }
   }
