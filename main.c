cb407880c3e702551fa1068b7d0258351bbb57bc
wznmickey <first@wznmickey.com>
2022-09-20 23:50:01 +0800

    feat: allow command without argc

--- main.c
+++ main.c
@@ -1,15 +1,32 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 int main() {
   char input[2048];
+  char *arg[2048];
   while (true) {
     printf("mumsh $ ");
     memset(input, 0, sizeof(char) * 2048);
     fgets(input, 2048 - 1, stdin);
-    if (strcmp(input, "exit\n") == 0) {
+    input[strlen(input) - 1] = '\0'; // remove the \n at the end.
+    if (strcmp(input, "exit") == 0) {
       printf("exit\n");
       return 0;
+    }
+    arg[0] = input;
+    arg[1] = NULL;
+    pid_t pid = fork();
+    if (pid == 0) {
+      int status_code = execvp(input, arg);
+      if (status_code == -1) {
+        printf("Command wrong with error code %d.", status_code);
+        return status_code;
+      }
+    } else {
+      waitpid(pid, NULL, 0);
     }
   }
 }
