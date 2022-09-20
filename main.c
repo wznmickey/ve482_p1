d70f76c15ec44a1c13b63d6e43dd37978091d8e1
wznmickey <first@wznmickey.com>
2022-09-20 22:59:03 +0800

    feat: allow loop and command `exit`

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+int main() {
+  char input[2048];
+  while (true) {
+    printf("mumsh $");
+    memset(input, 0, sizeof(char) * 2048);
+    fgets(input, 2048 - 1, stdin);
+    if (strcmp(input, "exit\n") == 0) {
+      return 0;
+    }
+  }
+}
